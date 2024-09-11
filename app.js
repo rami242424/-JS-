d6ece1a12ac1b48a1a7ef92fe9e89158abcc032f
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-11 19:33:46 +0900

    캐릭터 만들어보기

--- app.js
+++ app.js
@@ -3,14 +3,20 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
+ctx.fillRect(200, 200, 15, 100);
+ctx.fillRect(400, 200, 15, 100);
+ctx.fillRect(280, 200, 60, 200);
 
-ctx.fillRect(200, 200, 200, 20);
+ctx.arc(310, 150, 50, 0, 2*Math.PI);
+ctx.fill();
 
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200)
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(290, 150, 10, 0, 2*Math.PI);
+ctx.arc(325, 150, 10, 0, 2*Math.PI);
+ctx.fill();
+
+ctx.beginPath();
+ctx.fillStyle = "red";
+ctx.arc(308, 180, 8, 0, 1*Math.PI);
 ctx.fill();
