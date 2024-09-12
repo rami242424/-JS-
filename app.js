25e2e415561067950ec94c4a4654ecf497b728da
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-12 21:36:57 +0900

    클릭으로 단순한 자동선 따기

--- app.js
+++ app.js
@@ -3,20 +3,10 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 15, 100);
-ctx.fillRect(400, 200, 15, 100);
-ctx.fillRect(280, 200, 60, 200);
-
-ctx.arc(310, 150, 50, 0, 2*Math.PI);
-ctx.fill();
-
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(290, 150, 10, 0, 2*Math.PI);
-ctx.arc(325, 150, 10, 0, 2*Math.PI);
-ctx.fill();
-
-ctx.beginPath();
-ctx.fillStyle = "red";
-ctx.arc(308, 180, 8, 0, 1*Math.PI);
-ctx.fill();
+ctx.lineWidth = 2;
+function onClick(event){
+    // console.log(event);
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
+canvas.addEventListener("click", onClick);
