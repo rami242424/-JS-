1a82ea3243aef5880f4776eaaef8ff3ee0384cbe
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-12 23:35:25 +0900

    2.2 Line Width

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
 
@@ -12,6 +13,7 @@ function onMove(event){
         ctx.stroke();
         return;
     }
+    // ctx.beginPath();
     ctx.moveTo(event.offsetX, event.offsetY);
 }
 
@@ -22,7 +24,17 @@ function startPainting(){
 function cancelPainting(){
     isPainting = false;
 }
+
+function onLineWidthChange(event){
+    // console.log(event.target.value);
+    ctx.beginPath();
+    ctx.lineWidth = event.target.value;
+}
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
+// 그리면서 틀 밖으로 나갔다가 들어오면 클릭없이도 계속선이 그어지는 에러 해결코드
 canvas.addEventListener("mouseleave", cancelPainting);
+
+// range가 변하는 것을 감지할 eventListener
+lineWidth.addEventListener("change", onLineWidthChange);
